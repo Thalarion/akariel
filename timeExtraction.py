71859cb46e1c82166e71ff4363b3aaf0bc629366
Thalarion <66262833+Thalarion@users.noreply.github.com>
2020-06-01 16:57:53 +0200

    bare-bones solution in python

---
+++ timeExtraction.py
@@ -0,0 +1,20 @@
+import re
+
+regex12or24hour = r'(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9](AM|PM)|([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]'
+
+input_text = 'blabla 01:42:12PM bla bla blablabla bla'
+
+for timestamp in re.finditer(regex12or24hour, input_text.upper()):
+    item = timestamp.group(0)
+    if item.find("AM") == 8:
+        if item.find("12") == 0:
+            item = "00" + item[2:8]
+        else:
+            item = item[0:8]
+    elif item.find("PM") == 8:
+        if item.find("12") == 0:
+            item = item[0:8]
+        else:
+            hours = int(item[0:2]) + 12
+            item = str(hours) + item[2:8]
+    print(item)
